9aa82c2e7e30ac5cf55a716b6b3d832c680023ec
Besart Bara <besartbara@Besarts-MacBook-Air.local>
2019-05-29 11:28:03 +0200

    final commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+/** document.getElementsByClassName returns an array-like object of all child elements which have all of the given class names. In your case you should modify your code like this. getElementsByClassName returns a HTMLCollection which doesn't have classList property, you should iterate through the collection.**/
+
+
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
